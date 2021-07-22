8a76c92238db971421755b99a0fcdcf213fdb38c
Rohan Dudeja <rohanddj@gmail.com>
2021-07-22 18:11:38 +0530

    Day2 Exercise2 Solution

---
+++ day2/exercise2/solution.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+)
+
+func main() {
+	var wg sync.WaitGroup
+	var sum uint32
+	for student := 1; student <= 200; student++ {
+		wg.Add(1)
+		go func(){
+			atomic.AddUint32(&sum, uint32(rand.Intn(10)))
+			wg.Done()
+		}()
+		// wg.Done() // doubt: using wg.Done() here gives random output why!?
+	}
+	wg.Wait()
+	var average uint32
+	average = sum/200
+	fmt.Println(average)
+}
